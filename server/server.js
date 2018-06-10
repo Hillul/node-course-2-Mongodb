d3ad1213758f14615913cc45557f59fa87248b3d
Hillul Saikia <hillul2013@gmail.com>
2018-06-10 14:30:35 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,74 @@
+//Getting individual Resource-GET todos
 
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb')
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+	//console.log(req.body);
+	var todo=new Todo({
+		text:req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+//GET/todos/1234324
+app.get('/todos/:id',(req,res)=>{
+	//res.send(req.params);
+	var id=req.params.id;
+	//Valid id using isValid 
+	 //404 -send back empty send
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+	 //findById
+	 	//Success
+	 		// if todo - send it back
+	 		// if no todo - ssend 404 with empty body 
+	 	  //error 
+	 	    //400 -and send empty body back
+	 Todo.findById(id).then((todo)=>{
+	 	if(!todo){
+	 		return res.status(404).send();
+	 	}
+	 	res.send({todo});
+	 }).catch((e)=>{
+	 	res.status(400).send();
+	 });
+
+});
+
+
+app.listen(3000,()=>{
+	console.log('Starting on port 3000');
+});
+
+module.exports = {app};
+
+
+/*
+//testing get todos
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -37,6 +107,49 @@ app.listen(3000,()=>{
 });
 
 module.exports = {app};
+
+*/
+/*
+//List Resource get todos
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+	//console.log(req.body);
+	var todo=new Todo({
+		text:req.body.text
+	});
+
+	todo.save().then((doc)=>{
+		res.send(doc);
+	},(e)=>{
+		res.status(400).send(e);
+	});
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+
+app.listen(3000,()=>{
+	console.log('Starting on port 3000');
+});
+
+module.exports = {app};
+*/
 /*
 //test 0f post todos
 
