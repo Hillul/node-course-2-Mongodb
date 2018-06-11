d4b4bde086181a9c48c65019d142c2e79dbfdb72
Hillul Saikia <hillul2013@gmail.com>
2018-06-11 11:38:14 +0530

    Update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,4 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports={
-	mongoose:mongoose
-};
+module.exports={ mongoose };
